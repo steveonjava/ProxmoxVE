e6afdf6c905c451ac874a59953875153af1661d9
Stephen Chin <steveonjava@gmail.com>
2026-02-21 17:43:12 -0800

    Set VERBOSE=yes in protonmail-bridge.sh for verbose output

--- ct/protonmail-bridge.sh
+++ ct/protonmail-bridge.sh
@@ -5,6 +5,8 @@
 # Source: https://github.com/ProtonMail/proton-bridge
 # Description: Debian LXC that runs Proton Mail Bridge headless and exposes IMAP/SMTP to the LAN via socat.
 
+export VERBOSE=yes
+
 source <(curl -fsSL https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 
 APP="Proton Mail Bridge"
