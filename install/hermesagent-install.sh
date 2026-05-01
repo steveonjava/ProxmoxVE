1653c8ffe025405d5256e58456570db18265b181
Stephen Chin <steveonjava@gmail.com>
2026-05-01 15:02:40 -0700

    chore: remove redundant HERMES_HOME env var from installer (passed via --hermes-home arg)

--- install/hermesagent-install.sh
+++ install/hermesagent-install.sh
@@ -26,7 +26,6 @@ msg_info "Installing Hermes Agent"
 env \
 	HOME=/home/hermes \
 	PATH=/home/hermes/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin \
-	HERMES_HOME=/home/hermes/.hermes \
 	bash -lc 'curl -fsSL https://hermes-agent.nousresearch.com/install.sh | bash -s -- --skip-setup --hermes-home /home/hermes/.hermes --dir /home/hermes/.hermes/hermes-agent'
 
 if [[ ! -x /home/hermes/.local/bin/hermes ]]; then
